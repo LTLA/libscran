97281dd6e9602eecb6ef78dd350c26f55075f4e7
LTLA <LTLA@users.noreply.github.com>
2021-08-04 07:25:20 +0000

    Deploying to gh-pages from @ LTLA/libscran@051026d6ed42b5f4c79830b3a5f51dcfd48d87a9 🚀

--- docs/search/all_8.js
+++ docs/search/all_8.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['max_23',['max',['../structscran_1_1ClusterSNNGraph_1_1MultiLevelResult.html#ac157220e34f7746a8fe2d94a25e0ea92',1,'scran::ClusterSNNGraph::MultiLevelResult']]],
-  ['means_24',['means',['../structscran_1_1ModelGeneVar_1_1Results.html#a30c93b46a06b60773cfe1cbb895c4f32',1,'scran::ModelGeneVar::Results']]],
+  ['means_24',['means',['../structscran_1_1ModelGeneVar_1_1Results.html#a4528b71b2b1fd8dd58050b6681a7fd17',1,'scran::ModelGeneVar::Results']]],
   ['membership_25',['membership',['../structscran_1_1ClusterSNNGraph_1_1MultiLevelResult.html#aa69ddee2f4c685aad6b39d7bea97758f',1,'scran::ClusterSNNGraph::MultiLevelResult']]],
   ['modelgenevar_26',['ModelGeneVar',['../classscran_1_1ModelGeneVar.html',1,'scran']]],
   ['modularity_27',['modularity',['../structscran_1_1ClusterSNNGraph_1_1MultiLevelResult.html#a4767412b6f3a3df07bcf5da9ee5ea591',1,'scran::ClusterSNNGraph::MultiLevelResult']]],
--- docs/search/variables_a.js
+++ docs/search/variables_a.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['variances_132',['variances',['../structscran_1_1ModelGeneVar_1_1Results.html#aebc0aed5c5e50f0471766b6dc870fc58',1,'scran::ModelGeneVar::Results']]]
+  ['variances_132',['variances',['../structscran_1_1ModelGeneVar_1_1Results.html#ab23cd1fe1bad4b99da9182c46bc7ff5c',1,'scran::ModelGeneVar::Results']]]
 ];
