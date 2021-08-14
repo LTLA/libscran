01f9f4cfae433a31db5422449688a22d825414ba
LTLA <LTLA@users.noreply.github.com>
2021-08-14 17:28:03 +0000

    Deploying to gh-pages from @ LTLA/libscran@f5eed64177cba07febde91954d9ffdb95f17ef94 🚀

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -5,7 +5,7 @@ var searchData=
   ['filter_5fby_5fsums_10',['filter_by_sums',['../structscran_1_1PerCellQCFilters_1_1Results.html#a2f554d994ddf7898c86b743d1db3ccfe',1,'scran::PerCellQCFilters::Results']]],
   ['filtercells_11',['FilterCells',['../classscran_1_1FilterCells.html',1,'scran']]],
   ['filtercells_2ehpp_12',['FilterCells.hpp',['../FilterCells_8hpp.html',1,'']]],
-  ['fitted_13',['fitted',['../structscran_1_1FitTrendVar_1_1Results.html#a69eb248ad5726dc6cd0397153fd285f7',1,'scran::FitTrendVar::Results::fitted()'],['../structscran_1_1ModelGeneVar_1_1Results.html#af6a108e57f77e93c90b36e2bc968e9e0',1,'scran::ModelGeneVar::Results::fitted()']]],
+  ['fitted_13',['fitted',['../structscran_1_1FitTrendVar_1_1Results.html#a69eb248ad5726dc6cd0397153fd285f7',1,'scran::FitTrendVar::Results::fitted()'],['../structscran_1_1ModelGeneVar_1_1Results.html#a64a2610c001a2a71ffba3bc94df8b6a2',1,'scran::ModelGeneVar::Results::fitted()']]],
   ['fittrendvar_14',['FitTrendVar',['../classscran_1_1FitTrendVar.html',1,'scran']]],
   ['fittrendvar_2ehpp_15',['FitTrendVar.hpp',['../FitTrendVar_8hpp.html',1,'']]]
 ];
