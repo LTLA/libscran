4f2385dd9e96bf296543d50254fe99b66eaf0033
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-09-30 23:37:45 -0700

    Added typedef to support k-means clustering in the scran namespace. Explicitly include the kmeans library as a dependency.

---
+++ include/scran/clustering/ClusterKmeans.hpp
@@ -0,0 +1,23 @@
+#ifndef SCRAN_KMEANSCLUSTERING_HPP
+#define SCRAN_KMEANSCLUSTERING_HPP
+
+#include "kmeans/Kmeans.hpp"
+
+/**
+ * @file ClusterKmeans.hpp
+ *
+ * @brief Identify clusters of cells with k-means.
+ */
+
+namespace scran {
+
+/**
+ * Provides the `kmeans::Kmeans` class under the **scran** namespace.
+ * This is mostly for the sake of having a consistent naming scheme - 
+ * see the [**kmeans** documentation](https://ltla.github.io/CppKmeans/) for specifics.
+ */
+typedef kmeans::Kmeans ClusterKmeans;
+
+}
+
+#endif
