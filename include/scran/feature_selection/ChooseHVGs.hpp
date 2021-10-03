77f0eca30e0ea4d00433f0379ac0e70f48019dc1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-10-03 00:59:05 -0700

    Added a small utility to choose the top HVGs.

---
+++ include/scran/feature_selection/ChooseHVGs.hpp
@@ -0,0 +1,95 @@
+#ifndef SCRAN_CHOOSE_HVGS_HPP
+#define SCRAN_CHOOSE_HVGS_HPP
+
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cstdint>
+
+/**
+ * @file ChooseHVGs.hpp
+ *
+ * Choose highly variable genes for downstream analyses.
+ */
+
+namespace scran {
+
+/**
+ * @brief Choose highly variable genes for downstream analyses.
+ *
+ * This is done by selecting the `top` number of genes with the largest values for a precomputed variance statistic.
+ * We typically use the residual from the trend as computed by `ModelGeneVar`.
+ */
+class ChooseHVGs {
+public:
+    /**
+     * @brief Default paramater settings.
+     */
+    struct Defaults {
+        /**
+         * See `set_top()` for more details.
+         */
+        static constexpr size_t top = 4000;
+    };
+
+private:
+    size_t top = Defaults::top;
+
+public:
+    /**
+     * @param t The number of top genes to consider as highly variable genes.
+     *
+     * @return A reference to this `ChooseHVGs` object.
+     *
+     * The choice is more or less arbitrary and is a trade-off between capturing more biological signal at the cost of increasing noise.
+     * Values of 1000-5000 seem appropriate for most use cases.
+     * Note that increasing the number of genes will also increase the size of the subdataset to be used in downstream computation.
+     */
+    ChooseHVGs& set_top(size_t t = Defaults::top) {
+        top = t;
+        return *this;
+    }
+
+public:
+    /**
+     * @tparam V Type of the variance statistic.
+     * @tparam T Type to be used as a boolean.
+     *
+     * @param n Number of genes.
+     * @param[in] statistic Pointer to an array of length `n` containing the per-gene variance statistics.
+     * @param[out] output Pointer to an array of length `n`, used to store a boolean flag.
+     *
+     * @return `output` is filled with `true` if the gene is to be retained and `false` otherwise.
+     */
+    template<typename V, typename T>
+    void run(size_t n, const V* statistic, T* output) const {
+        std::vector<size_t> collected(n);
+        std::iota(collected.begin(), collected.end(), 0);
+        std::sort(collected.begin(), collected.end(), [&](size_t l, size_t r) -> bool { return statistic[l] > statistic[r]; });
+        
+        auto limit = std::min(n, top);
+        std::fill(output, output + n, false);
+        for (size_t i = 0; i < limit; ++i) {
+            output[collected[i]] = true; 
+        }
+    }
+
+    /**
+     * @tparam V Type of the variance statistic.
+     * @tparam T Type to be used as a boolean.
+     *
+     * @param n Number of genes.
+     * @param[in] statistic Pointer to an array of length `n` containing the per-gene variance statistics.
+     *
+     * @return A vector of booleans of length `n`, indicating whether each gene is to be retained.
+     */
+    template<typename T = uint8_t, typename V>
+    std::vector<T> run(size_t n, const V* statistic) const {
+        std::vector<T> output(n);
+        run(n, statistic, output.data());
+        return output;
+    }
+};
+
+}
+#endif
---
+++ tests/src/feature_selection/ChooseHVGs.cpp
@@ -0,0 +1,44 @@
+#include <gtest/gtest.h>
+
+#include "scran/feature_selection/ChooseHVGs.hpp"
+#include "aarand/aarand.hpp"
+#include <random>
+
+class ChooseHVGsTest : public ::testing::TestWithParam<std::tuple<int, int> > {};
+
+TEST_P(ChooseHVGsTest, Basic) {
+    auto p = GetParam();
+    size_t ngenes = std::get<0>(p);
+    size_t ntop = std::get<1>(p);
+
+    // Setting up the test.
+    std::mt19937_64 rng(42);
+    std::vector<double> x (ngenes);
+    for (auto& x0 : x) { x0 = aarand::standard_uniform(rng); }
+
+    scran::ChooseHVGs chooser;
+    chooser.set_top(ntop);
+    auto output = chooser.run(ngenes, x.data());
+
+    // Checking that everything is in order.
+    EXPECT_EQ(std::min(ngenes, ntop), std::accumulate(output.begin(), output.end(), 0)); 
+
+    double min_has = 100, max_lost = 0;
+    for (size_t o = 0; o < ngenes; ++o) {
+        if (output[o]) {
+            min_has = std::min(min_has, x[o]);
+        } else {
+            max_lost = std::max(max_lost, x[o]);
+        }
+    }
+    EXPECT_TRUE(min_has > max_lost);
+}
+
+INSTANTIATE_TEST_CASE_P(
+    ChooseHVGs,
+    ChooseHVGsTest,
+    ::testing::Combine(
+        ::testing::Values(11, 111, 1111), // number of values
+        ::testing::Values(5, 50, 500) // number of tops
+    )
+);
