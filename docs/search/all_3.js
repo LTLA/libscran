c862cbbdeee5679aa2fe3a81e16724ed17f518e9
LTLA <LTLA@users.noreply.github.com>
2021-10-16 23:18:31 +0000

    Deploying to gh-pages from @ LTLA/libscran@467389ca9b539aa170c6c8a8cc4ea129242eb246 🚀

--- docs/search/all_3.js
+++ docs/search/all_3.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['defaults_0',['Defaults',['../structscran_1_1BuildSNNGraph_1_1Defaults.html',1,'scran::BuildSNNGraph::Defaults'],['../structscran_1_1ChooseHVGs_1_1Defaults.html',1,'scran::ChooseHVGs::Defaults'],['../structscran_1_1ClusterSNNGraph_1_1Defaults.html',1,'scran::ClusterSNNGraph::Defaults']]],
+  ['defaults_0',['Defaults',['../structscran_1_1BuildSNNGraph_1_1Defaults.html',1,'scran::BuildSNNGraph::Defaults'],['../structscran_1_1ChooseHVGs_1_1Defaults.html',1,'scran::ChooseHVGs::Defaults'],['../structscran_1_1ClusterSNNGraph_1_1Defaults.html',1,'scran::ClusterSNNGraph::Defaults'],['../structscran_1_1ScoreMarkers_1_1Defaults.html',1,'scran::ScoreMarkers::Defaults']]],
   ['destroy_1',['destroy',['../structscran_1_1ClusterSNNGraph_1_1Graph.html#a0d7cf0747ed7bff0c4e810dcbd77ebdb',1,'scran::ClusterSNNGraph::Graph']]],
   ['detected_2',['detected',['../structscran_1_1PerCellQCFilters_1_1Thresholds.html#a1e6c284b8aee5fefbf8cb762f49962ee',1,'scran::PerCellQCFilters::Thresholds::detected()'],['../structscran_1_1PerCellQCMetrics_1_1Results.html#a2961184e3b2a0193afb39440eaf6e712',1,'scran::PerCellQCMetrics::Results::detected()']]]
 ];
