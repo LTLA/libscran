9ae682c7b9b7b9841f821bc6167aa227842f5adc
LTLA <LTLA@users.noreply.github.com>
2021-10-16 06:39:04 +0000

    Deploying to gh-pages from @ LTLA/libscran@5c58ac10aa2c8f252779ef50e543023b34ddedb5 🚀

--- docs/search/all_e.js
+++ docs/search/all_e.js
@@ -1,7 +1,6 @@
 var searchData=
 [
-  ['thresholds_0',['Thresholds',['../structscran_1_1IsOutlier_1_1Thresholds.html',1,'scran::IsOutlier::Thresholds'],['../structscran_1_1PerCellQCFilters_1_1Thresholds.html',1,'scran::PerCellQCFilters::Thresholds']]],
+  ['thresholds_0',['Thresholds',['../structscran_1_1IsOutlier_1_1Thresholds.html',1,'scran::IsOutlier::Thresholds'],['../structscran_1_1PerCellQCFilters_1_1Thresholds.html',1,'scran::PerCellQCFilters::Thresholds'],['../structscran_1_1IsOutlier_1_1Thresholds.html#a20a0eedf427f07d34c78aee7e07fe839',1,'scran::IsOutlier::Thresholds::Thresholds()']]],
   ['thresholds_1',['thresholds',['../structscran_1_1IsOutlier_1_1Results.html#ab76c89d333a817bf7881d4eb2ba737bb',1,'scran::IsOutlier::Results::thresholds()'],['../structscran_1_1PerCellQCFilters_1_1Results.html#ad3ab33eeb778199dc2c698d429099063',1,'scran::PerCellQCFilters::Results::thresholds()']]],
-  ['thresholds_2',['Thresholds',['../structscran_1_1IsOutlier_1_1Thresholds.html#a20a0eedf427f07d34c78aee7e07fe839',1,'scran::IsOutlier::Thresholds']]],
-  ['top_3',['top',['../structscran_1_1ChooseHVGs_1_1Defaults.html#ab47b622404c5eb3ed1835518e4164bf1',1,'scran::ChooseHVGs::Defaults']]]
+  ['top_2',['top',['../structscran_1_1ChooseHVGs_1_1Defaults.html#ab47b622404c5eb3ed1835518e4164bf1',1,'scran::ChooseHVGs::Defaults']]]
 ];
