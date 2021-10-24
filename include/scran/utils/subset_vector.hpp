b4eb0d2e52fb70e675da6961c019f153ba92b640
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-10-23 23:54:05 -0700

    Added utility to easily subset vectors.

---
+++ include/scran/utils/subset_vector.hpp
@@ -0,0 +1,53 @@
+#ifndef SUBSET_VECTOR_HPP
+#define SUBSET_VECTOR_HPP
+
+namespace scran {
+
+/**
+ * Subset a vector to retain/discard elements.
+ *
+ * @tparam retain Should the non-zero elements in `sub` be retained (`true`) or discarded (`false`)?
+ * @tparam Vector A vector class that has a `size()` method, a `[]` operator, and a constructor that initializes the vector to a specified length.
+ * @tparam Subset Integer/boolean type for the subsetting vector.
+ *
+ * @param vec A vector of arbitrary (copy-able) elements.
+ * @param sub Pointer to an array of integer/boolean elements of length equal to `vec`.
+ * Non-zero values indicate that an element should be retained (if `retain=true`) or discarded (otherwise).
+ *
+ * @return A vector containing the desired subset of elements in `vec`.
+ */
+template<bool retain, class Vector, typename Subset>
+Vector subset_vector(const Vector& vec, const Subset* sub) {
+    int n = 0;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if (sub[i]) {
+            ++n;
+        }
+    }
+
+    if constexpr(!retain) {
+        n = vec.size() - n;        
+    }
+
+    Vector output(n);
+    size_t counter = 0;
+    for (size_t i = 0; i < vec.size(); ++i) {
+        if constexpr(retain) {
+            if (sub[i]) {
+                output[counter] = vec[i];
+                ++counter;
+            }
+        } else {
+            if (!sub[i]) {
+                output[counter] = vec[i];
+                ++counter;
+            }
+        }
+    }
+
+    return output;
+}
+
+}
+
+#endif
---
+++ tests/src/utils/subset_vector.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "scran/utils/subset_vector.hpp"
+#include <vector>
+
+TEST(SubsetVector, Simple) {
+    std::vector<double> input{1, 2, 3, 4, 5};
+    std::vector<int> subset{0, 1, 0, 1, 0};
+
+    std::vector<double> retained{2, 4};
+    EXPECT_EQ(retained, scran::subset_vector<true>(input, subset.data()));
+
+    std::vector<double> discarded{1, 3, 5};
+    EXPECT_EQ(discarded, scran::subset_vector<false>(input, subset.data()));
+}
