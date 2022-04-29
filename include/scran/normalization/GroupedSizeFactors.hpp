445cce1e5534af4b4f0d2170f8724c05412dfc3a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-04-29 09:25:20 -0700

    Replaced the PCA-based method with edgeR's sum of square roots. Fleshed out the tests, fixed the tatami version again.

--- include/scran/normalization/GroupedSizeFactors.hpp
+++ include/scran/normalization/GroupedSizeFactors.hpp
@@ -2,9 +2,7 @@
 #define SCRAN_CLUSTERED_SIZE_FACTORS_HPP
 
 #include "MedianSizeFactors.hpp"
-#include "LogNormCounts.hpp"
 #include "../aggregation/AggregateAcrossCells.hpp"
-#include "../dimensionality_reduction/RunPCA.hpp"
 
 #include "tatami/base/DenseMatrix.hpp"
 #include "tatami/ext/ArrayView.hpp"
@@ -17,6 +15,33 @@
 namespace scran {
 
 class GroupedSizeFactors {
+public:
+    /**
+     * @brief Default parameter settings.
+     */
+    struct Defaults {
+        /** 
+         * See `set_center()` for more details.
+         */
+        static constexpr bool center = true;
+    };
+
+    /**
+     * @param c Whether to center the size factors to have a mean of unity.
+     * This is usually desirable for interpretation of relative values.
+     * 
+     * @return A reference to this `MedianSizeFactors` object.
+     *
+     * For more control over centering, this can be set to `false` and the resulting size factors can be passed to `CenterSizeFactors`.
+     */
+    GroupedSizeFactors& set_center(bool c = Defaults::center) {
+        center = c;
+        return *this;
+    }
+
+private:
+    bool center = Defaults::center;
+
 public:
     template<typename T, typename IDX, typename Group, typename Out>
     void run(const tatami::Matrix<T, IDX>* mat, const Group* group, Out* output) {
@@ -37,57 +62,57 @@ public:
             aggregator.run(mat, group, std::move(sums), std::vector<int*>());
         }
 
-        tatami::DenseColumnMatrix<T, IDX> aggmat(NR, ngroups, tatami::ArrayView(combined.data(), combined.size()));
-        auto aggcolsums = tatami::column_sums(&aggmat);
+        // Choosing one of them to be the reference. Here, we borrow some logic
+        // from edgeR and use the one with the largest sum of square roots,
+        // which provides a compromise between transcriptome coverage and
+        // complexity. The root ensures that we don't pick a sample that just
+        // has very high expression in a small subset of genes, while still
+        // remaining responsive to the overall coverage level.
+        size_t ref = 0;
+        double best = 0;
 
-        // Choosing one of them to be the reference. Here, we pick the group
-        // closest to the middle of the first PC, on the understanding that
-        // this is the most "intermediate" and has the fewest DE compared to
-        // other groups. This reduces the estimation error on the factors. 
-        auto copy = combined;
-        size_t closest = 0;
-
-        {
-            LogNormCounts logger;
-            auto logged = logger.run(std::shared_ptr<const tatami::Matrix<T, IDX> >(&mat, [](const tatami::Matrix<T,IDX>*){}), aggcolsums);
-
-            RunPCA pca;
-            auto pres = pca.set_rank(1).run(logged);
-
-            auto middle = std::accumulate(pres.pcs.begin(), pres.pcs.end(), 0) / NC;
-            double closest_diff;
-
-            for (size_t i = 0; i < NC; ++i) {
-                double absdiff = std::abs(pres.pcs[i] - middle);
-                if (i == 0 || absdiff < closest_diff) {
-                    closest_diff = absdiff;
-                    closest = i;
-                }
+        for (size_t i = 0; i < ngroups; ++i) {
+            auto start = combined.data() + i * NR;
+            double current = 0;
+            for (size_t j = 0; j < NR; ++j) {
+                current += std::sqrt(start[j]);
+            }
+            if (current > best) {
+                ref = i;
             }
         }
 
         // Computing median-based size factors.
+        tatami::ArrayView view(combined.data(), combined.size());
+        tatami::DenseColumnMatrix<T, IDX, decltype(view)> aggmat(NR, ngroups, std::move(view));
         MedianSizeFactors med;
-        auto mres = med.run(&aggmat, combined.data() + closest * NR);
+        auto mres = med.run(&aggmat, combined.data() + ref * NR);
 
         // Propagating to each cell via library size-based normalization.
+        auto aggcolsums = tatami::column_sums(&aggmat);
         auto colsums = tatami::column_sums(mat);
         for (size_t i = 0; i < NC; ++i) {
             auto curgroup = group[i];
             auto scale = static_cast<double>(colsums[i])/static_cast<double>(aggcolsums[curgroup]);
             output[i] = scale * mres.factors[curgroup];
         }
+
+        // Throwing in some centering.
+        if (center) {
+            CenterSizeFactors centerer;
+            centerer.run(NC, output);
+        }
     }
 
 public:
-    template<typename Output>
+    template<typename Out>
     struct Result {
         Result(size_t NC) : factors(NC) {}
-        std::vector<Output> factors;
+        std::vector<Out> factors;
     };
 
     template<typename Out = double, typename T, typename IDX, typename Group>
-    void run(const tatami::Matrix<T, IDX>* mat, const Group* group) {
+    Result<Out> run(const tatami::Matrix<T, IDX>* mat, const Group* group) {
         Result<Out> output(mat->ncol());
         run(mat, group, output.factors.data());
         return output;
--- tests/src/normalization/GroupedSizeFactors.cpp
+++ tests/src/normalization/GroupedSizeFactors.cpp
@@ -5,8 +5,124 @@
 
 #include "tatami/base/DenseMatrix.hpp"
 #include "tatami/stats/sums.hpp"
-
 #include "scran/normalization/GroupedSizeFactors.hpp"
 
 #include <cmath>
+#include <random>
 
+TEST(GroupedSizeFactors, LibSize) {
+    size_t NR = 100, NC = 10;
+
+    // Just returns library size factors when there's one group.
+    std::vector<double> contents(NR * NC);
+    std::vector<double> multiplier(NC);
+    {
+        std::mt19937_64 rng(1000);
+        auto cIt = contents.begin();
+        for (size_t c = 0; c < NC; ++c) {
+            double& mult = multiplier[c];
+            mult = static_cast<double>(rng() % 100 + 1)/10; // true scaling factor.
+            for (size_t r = 0; r < NR; ++r, ++cIt) {
+                *cIt = mult * (r * 100 + rng() % 100); // increasing gene abundance.
+            }
+        }
+    }
+    tatami::DenseColumnMatrix<double> mat(NR, NC, contents);
+
+    scran::GroupedSizeFactors grouper;
+    std::vector<int> groups(NC);
+    auto res = grouper.run(&mat, groups.data());
+
+    auto sums = tatami::column_sums(&mat);
+    scran::CenterSizeFactors center;
+    center.run(sums.size(), sums.data());
+
+    int diffs = 0;
+    for (size_t c = 0; c < NC; ++c) {
+        diffs += (std::abs(1 - sums[c] / res.factors[c]) > 0.00000001);
+    }
+    EXPECT_TRUE(diffs == 0);
+}
+
+TEST(GroupedSizeFactors, ByGroupLibSize) {
+    size_t NR = 1000, NC = 100;
+    size_t ngroups = 3;
+
+    std::vector<double> contents(NR * NC);
+    std::vector<int> groups(NC);
+    {
+        std::mt19937_64 rng(1001);
+        auto cIt = contents.begin();
+        for (size_t c = 0; c < NC; ++c) {
+            groups[c] = c % ngroups;
+            int scaling = groups[c] + 1; // group-specific scaling.
+            for (size_t r = 0; r < NR; ++r, ++cIt) {
+                *cIt = scaling * (r * 100 + rng() % 100); // increasing gene abundance.
+            }
+        }
+    }
+    tatami::DenseColumnMatrix<double> mat(NR, NC, contents);
+
+    scran::GroupedSizeFactors grouper;
+    auto res = grouper.run(&mat, groups.data());
+
+    // Averages should match the groups.
+    std::vector<double> averages(ngroups);
+    std::vector<int> frequency(ngroups);
+    for (size_t i = 0; i < res.factors.size(); ++i) {
+        averages[groups[i]] += res.factors[i];
+        ++frequency[groups[i]];
+    }
+
+    for (size_t j = 0; j < ngroups; ++j) {
+        averages[j] /= frequency[j];
+        if (j) {
+            double ratio = averages[j] / averages[0];
+            EXPECT_FALSE(std::abs(1 - ratio / (j + 1)) > 0.001);
+        }
+    }
+}
+
+TEST(GroupedSizeFactors, ByGroupComposition) {
+    size_t NR = 1000, NC = 100;
+    size_t ngroups = 3;
+
+    std::vector<double> contents(NR * NC);
+    std::vector<int> groups(NC);
+    {
+        std::mt19937_64 rng(1002);
+        auto cIt = contents.begin();
+        for (size_t c = 0; c < NC; ++c) {
+            groups[c] = c % ngroups;
+            for (size_t r = 0; r < NR; ++r, ++cIt) {
+                if (groups[c] == 1 && r == 0) { // injecting strong DE into each group.
+                    *cIt = 10000;
+                } else if (groups[c] == 2 && r == 1) {
+                    *cIt = 1000000;
+                } else {
+                    *cIt = r + rng() % 10;
+                }
+            }
+        }
+    }
+    tatami::DenseColumnMatrix<double> mat(NR, NC, contents);
+
+    scran::GroupedSizeFactors grouper;
+    auto res = grouper.run(&mat, groups.data());
+
+    // Averages should be close to 1, once the composition bias is ignored.
+    std::vector<double> averages(ngroups);
+    std::vector<int> frequency(ngroups);
+    for (size_t i = 0; i < res.factors.size(); ++i) {
+        averages[groups[i]] += res.factors[i];
+        ++frequency[groups[i]];
+    }
+
+    for (size_t j = 0; j < ngroups; ++j) {
+        averages[j] /= frequency[j];
+        if (j) {
+            double ratio = averages[j] / averages[0];
+            EXPECT_FALSE(std::abs(1 - ratio) > 0.001);
+        }
+    }
+}
