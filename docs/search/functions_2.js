e377832cc6b315a8b77d21a543b5cdce03abb1b2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-05-16 19:53:18 +0000

    Deploying to gh-pages from @ LTLA/libscran@2151fbf68650bf8c1067329076e8de180b1e9c2d 🚀

--- docs/search/functions_2.js
+++ docs/search/functions_2.js
@@ -1,8 +1,9 @@
 var searchData=
 [
   ['combine_5ffactors_0',['combine_factors',['../classscran_1_1AggregateAcrossCells.html#a78bfad996edef3a958377c50dabdeff4',1,'scran::AggregateAcrossCells::combine_factors(size_t n, std::vector&lt; const Factor * &gt; factors, Combined *combined)'],['../classscran_1_1AggregateAcrossCells.html#ae36a7bc1ed0d40aa179c3a486a15c0ea',1,'scran::AggregateAcrossCells::combine_factors(size_t n, std::vector&lt; const Factor * &gt; factors)']]],
-  ['compute_5fall_5fsummaries_1',['compute_all_summaries',['../structscran_1_1ScoreMarkers_1_1Defaults.html#a220fdcc79f6d7c8810eacd29fbf19b09',1,'scran::ScoreMarkers::Defaults']]],
-  ['compute_5fdistance_2',['compute_distance',['../classscran_1_1ScaleByNeighbors.html#a61fd690cbc88b65016cef7fe4616a085',1,'scran::ScaleByNeighbors::compute_distance(int ndim, size_t ncells, const double *data) const'],['../classscran_1_1ScaleByNeighbors.html#a787ad5a20e61bd9ac930242840f72448',1,'scran::ScaleByNeighbors::compute_distance(const Search *search) const']]],
-  ['compute_5fno_5fsummaries_3',['compute_no_summaries',['../structscran_1_1ScoreMarkers_1_1Defaults.html#a2ce7d34d04baed541e958ac5deebfb36',1,'scran::ScoreMarkers::Defaults']]],
-  ['compute_5fscale_4',['compute_scale',['../classscran_1_1ScaleByNeighbors.html#acf1452d108cbd76ed4fd31e96a86bc7d',1,'scran::ScaleByNeighbors']]]
+  ['combine_5fscaled_5fembeddings_1',['combine_scaled_embeddings',['../classscran_1_1ScaleByNeighbors.html#ab2457f764f63b1a3874257b7dca97478',1,'scran::ScaleByNeighbors']]],
+  ['compute_5fall_5fsummaries_2',['compute_all_summaries',['../structscran_1_1ScoreMarkers_1_1Defaults.html#a220fdcc79f6d7c8810eacd29fbf19b09',1,'scran::ScoreMarkers::Defaults']]],
+  ['compute_5fdistance_3',['compute_distance',['../classscran_1_1ScaleByNeighbors.html#a61fd690cbc88b65016cef7fe4616a085',1,'scran::ScaleByNeighbors::compute_distance(int ndim, size_t ncells, const double *data) const'],['../classscran_1_1ScaleByNeighbors.html#a787ad5a20e61bd9ac930242840f72448',1,'scran::ScaleByNeighbors::compute_distance(const Search *search) const']]],
+  ['compute_5fno_5fsummaries_4',['compute_no_summaries',['../structscran_1_1ScoreMarkers_1_1Defaults.html#a2ce7d34d04baed541e958ac5deebfb36',1,'scran::ScoreMarkers::Defaults']]],
+  ['compute_5fscale_5',['compute_scale',['../classscran_1_1ScaleByNeighbors.html#a0c63ae4793732b14ae902bc165e3c968',1,'scran::ScaleByNeighbors::compute_scale(const std::vector&lt; std::pair&lt; double, double &gt; &gt; &amp;distances)'],['../classscran_1_1ScaleByNeighbors.html#a257b5d2f53b85a7769110c205d101be8',1,'scran::ScaleByNeighbors::compute_scale(const std::pair&lt; double, double &gt; &amp;ref, const std::pair&lt; double, double &gt; &amp;target)']]]
 ];
