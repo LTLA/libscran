aa5f8ba68b77a554c786dac00f4ce27099624c99
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-06-14 14:54:37 -0700

    Removed the old clean_up overload.

--- include/scran/dimensionality_reduction/pca_utils.hpp
+++ include/scran/dimensionality_reduction/pca_utils.hpp
@@ -9,22 +9,6 @@ namespace scran {
 
 namespace pca_utils {
 
-inline void clean_up(size_t NC, const Eigen::MatrixXd& U, const Eigen::VectorXd& D, Eigen::MatrixXd& pcs, Eigen::VectorXd& variance_explained) {
-    pcs = U;
-    for (int i = 0; i < U.cols(); ++i) {
-        for (size_t j = 0; j < NC; ++j) {
-            pcs(j, i) *= D[i];
-        }
-    }
-
-    variance_explained.resize(D.size());
-    for (int i = 0; i < D.size(); ++i) {
-        variance_explained[i] = D[i] * D[i] / static_cast<double>(NC - 1);
-    }
-
-    return;
-}
-
 inline void clean_up(size_t NC, Eigen::MatrixXd& U, Eigen::VectorXd& D) {
     auto uIt = U.data();
     auto dIt = D.data();
