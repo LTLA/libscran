83a6e1b13f5e72ff6c567441210faa288a42d379
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-07-21 09:23:44 -0700

    Updated kmeans version for explicit num_threads specification.

--- include/scran/clustering/ClusterKmeans.hpp
+++ include/scran/clustering/ClusterKmeans.hpp
@@ -15,6 +15,9 @@ namespace scran {
  * Provides the `kmeans::Kmeans` class under the **scran** namespace.
  * This is mostly for the sake of having a consistent naming scheme - 
  * see the [**kmeans** documentation](https://ltla.github.io/CppKmeans/) for specifics.
+ *
+ * Note that applications using a custom parallelization scheme will need to set the `KMEANS_CUSTOM_PARALLEL` macro;
+ * the setting of the `SCRAN_CUSTOM_PARALLEL` macro will not be propagated automatically.
  */
 typedef kmeans::Kmeans<> ClusterKmeans;
 
