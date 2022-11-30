faa4b1856949110e9a3c006f33e33b998b6668f9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-11-30 04:39:58 +0000

    Deploying to gh-pages from @ LTLA/libscran@1707107281632348209a21f62aacd382faa6c567 🚀

--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -1,17 +1,18 @@
 var searchData=
 [
-  ['center_0',['center',['../structscran_1_1LogNormCounts_1_1Defaults.html#aeb75e212cc28f620391082fc735e7341',1,'scran::LogNormCounts::Defaults::center()'],['../structscran_1_1MedianSizeFactors_1_1Defaults.html#af747d883c33f4793ce28b9aade2d2ad6',1,'scran::MedianSizeFactors::Defaults::center()'],['../structscran_1_1GroupedSizeFactors_1_1Defaults.html#acbe5c154e3b67420a1f2ef84ef304310',1,'scran::GroupedSizeFactors::Defaults::center()']]],
-  ['cohen_1',['cohen',['../structscran_1_1PairwiseEffects_1_1Results.html#a9d3790e1cd99eb3e0eee4e1bf3d8646f',1,'scran::PairwiseEffects::Results::cohen()'],['../structscran_1_1ScoreMarkers_1_1Results.html#aea2491f627de9c217b86ad888429e99d',1,'scran::ScoreMarkers::Results::cohen()']]],
-  ['compute_5fauc_2',['compute_auc',['../structscran_1_1PairwiseEffects_1_1Defaults.html#a909d4dc272c6ac0bec6069f5559b1962',1,'scran::PairwiseEffects::Defaults']]],
-  ['compute_5fcohen_3',['compute_cohen',['../structscran_1_1PairwiseEffects_1_1Defaults.html#a8a0050d343bc8b2eec80ce51982f6945',1,'scran::PairwiseEffects::Defaults']]],
-  ['compute_5fdelta_5fdetected_4',['compute_delta_detected',['../structscran_1_1PairwiseEffects_1_1Defaults.html#aed4f78128bfe84cd267010020a2c4f24',1,'scran::PairwiseEffects::Defaults']]],
-  ['compute_5fdetected_5',['compute_detected',['../structscran_1_1AggregateAcrossCells_1_1Defaults.html#a75c5c2a8222c372e3d6f419b3e1f8ffe',1,'scran::AggregateAcrossCells::Defaults']]],
-  ['compute_5flfc_6',['compute_lfc',['../structscran_1_1PairwiseEffects_1_1Defaults.html#ad9a3f0f122ec6f4ab5654c691d3a8af7',1,'scran::PairwiseEffects::Defaults']]],
-  ['compute_5fmax_7',['compute_max',['../structscran_1_1SummarizeEffects_1_1Defaults.html#a55d321e03e94bbaca62d75eb1cc10c81',1,'scran::SummarizeEffects::Defaults']]],
-  ['compute_5fmean_8',['compute_mean',['../structscran_1_1SummarizeEffects_1_1Defaults.html#af17ebeef3fbefc11fef6e3732c524f8c',1,'scran::SummarizeEffects::Defaults']]],
-  ['compute_5fmedian_9',['compute_median',['../structscran_1_1SummarizeEffects_1_1Defaults.html#a27953b061082adad335171f7d17cb023',1,'scran::SummarizeEffects::Defaults']]],
-  ['compute_5fmin_10',['compute_min',['../structscran_1_1SummarizeEffects_1_1Defaults.html#aef011af1e9bbcd7e606b4e16307e8212',1,'scran::SummarizeEffects::Defaults']]],
-  ['compute_5fmin_5frank_11',['compute_min_rank',['../structscran_1_1SummarizeEffects_1_1Defaults.html#af3511c0b77e50addde9d2b9fc782f39f',1,'scran::SummarizeEffects::Defaults']]],
-  ['compute_5fsums_12',['compute_sums',['../structscran_1_1AggregateAcrossCells_1_1Defaults.html#a4299b3f74f68fc088d4b2bacf77c5fee',1,'scran::AggregateAcrossCells::Defaults']]],
-  ['counts_13',['counts',['../structscran_1_1AggregateAcrossCells_1_1Combinations.html#aff25c122a815de09258f468ab4c03d70',1,'scran::AggregateAcrossCells::Combinations']]]
+  ['cache_5fsize_0',['cache_size',['../structscran_1_1ScoreMarkers_1_1Defaults.html#a0a887b918caa0be9523adf38ec6e892b',1,'scran::ScoreMarkers::Defaults']]],
+  ['center_1',['center',['../structscran_1_1GroupedSizeFactors_1_1Defaults.html#acbe5c154e3b67420a1f2ef84ef304310',1,'scran::GroupedSizeFactors::Defaults::center()'],['../structscran_1_1LogNormCounts_1_1Defaults.html#aeb75e212cc28f620391082fc735e7341',1,'scran::LogNormCounts::Defaults::center()'],['../structscran_1_1MedianSizeFactors_1_1Defaults.html#af747d883c33f4793ce28b9aade2d2ad6',1,'scran::MedianSizeFactors::Defaults::center()']]],
+  ['cohen_2',['cohen',['../structscran_1_1PairwiseEffects_1_1Results.html#a9d3790e1cd99eb3e0eee4e1bf3d8646f',1,'scran::PairwiseEffects::Results::cohen()'],['../structscran_1_1ScoreMarkers_1_1Results.html#aea2491f627de9c217b86ad888429e99d',1,'scran::ScoreMarkers::Results::cohen()']]],
+  ['compute_5fauc_3',['compute_auc',['../structscran_1_1PairwiseEffects_1_1Defaults.html#a909d4dc272c6ac0bec6069f5559b1962',1,'scran::PairwiseEffects::Defaults']]],
+  ['compute_5fcohen_4',['compute_cohen',['../structscran_1_1PairwiseEffects_1_1Defaults.html#a8a0050d343bc8b2eec80ce51982f6945',1,'scran::PairwiseEffects::Defaults']]],
+  ['compute_5fdelta_5fdetected_5',['compute_delta_detected',['../structscran_1_1PairwiseEffects_1_1Defaults.html#aed4f78128bfe84cd267010020a2c4f24',1,'scran::PairwiseEffects::Defaults']]],
+  ['compute_5fdetected_6',['compute_detected',['../structscran_1_1AggregateAcrossCells_1_1Defaults.html#a75c5c2a8222c372e3d6f419b3e1f8ffe',1,'scran::AggregateAcrossCells::Defaults']]],
+  ['compute_5flfc_7',['compute_lfc',['../structscran_1_1PairwiseEffects_1_1Defaults.html#ad9a3f0f122ec6f4ab5654c691d3a8af7',1,'scran::PairwiseEffects::Defaults']]],
+  ['compute_5fmax_8',['compute_max',['../structscran_1_1SummarizeEffects_1_1Defaults.html#a55d321e03e94bbaca62d75eb1cc10c81',1,'scran::SummarizeEffects::Defaults']]],
+  ['compute_5fmean_9',['compute_mean',['../structscran_1_1SummarizeEffects_1_1Defaults.html#af17ebeef3fbefc11fef6e3732c524f8c',1,'scran::SummarizeEffects::Defaults']]],
+  ['compute_5fmedian_10',['compute_median',['../structscran_1_1SummarizeEffects_1_1Defaults.html#a27953b061082adad335171f7d17cb023',1,'scran::SummarizeEffects::Defaults']]],
+  ['compute_5fmin_11',['compute_min',['../structscran_1_1SummarizeEffects_1_1Defaults.html#aef011af1e9bbcd7e606b4e16307e8212',1,'scran::SummarizeEffects::Defaults']]],
+  ['compute_5fmin_5frank_12',['compute_min_rank',['../structscran_1_1SummarizeEffects_1_1Defaults.html#af3511c0b77e50addde9d2b9fc782f39f',1,'scran::SummarizeEffects::Defaults']]],
+  ['compute_5fsums_13',['compute_sums',['../structscran_1_1AggregateAcrossCells_1_1Defaults.html#a4299b3f74f68fc088d4b2bacf77c5fee',1,'scran::AggregateAcrossCells::Defaults']]],
+  ['counts_14',['counts',['../structscran_1_1AggregateAcrossCells_1_1Combinations.html#aff25c122a815de09258f468ab4c03d70',1,'scran::AggregateAcrossCells::Combinations']]]
 ];
