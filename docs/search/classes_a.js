2a13ee8e3a82370501af0eff0d7ee0f83020544c
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-12-22 12:45:26 +0000

    Deploying to gh-pages from @ LTLA/libscran@70f35d8ae68848825274dcf9a44615b5b27064cd 🚀

--- docs/search/classes_a.js
+++ docs/search/classes_a.js
@@ -3,6 +3,7 @@ var searchData=
   ['scalebyneighbors_0',['ScaleByNeighbors',['../classscran_1_1ScaleByNeighbors.html',1,'scran']]],
   ['scoremarkers_1',['ScoreMarkers',['../classscran_1_1ScoreMarkers.html',1,'scran']]],
   ['suggestadtqcfilters_2',['SuggestAdtQcFilters',['../classscran_1_1SuggestAdtQcFilters.html',1,'scran']]],
-  ['suggestrnaqcfilters_3',['SuggestRnaQcFilters',['../classscran_1_1SuggestRnaQcFilters.html',1,'scran']]],
-  ['summarizeeffects_4',['SummarizeEffects',['../classscran_1_1SummarizeEffects.html',1,'scran']]]
+  ['suggestcrisprqcfilters_3',['SuggestCrisprQcFilters',['../classscran_1_1SuggestCrisprQcFilters.html',1,'scran']]],
+  ['suggestrnaqcfilters_4',['SuggestRnaQcFilters',['../classscran_1_1SuggestRnaQcFilters.html',1,'scran']]],
+  ['summarizeeffects_5',['SummarizeEffects',['../classscran_1_1SummarizeEffects.html',1,'scran']]]
 ];
