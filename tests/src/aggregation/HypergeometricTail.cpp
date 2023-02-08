aca4da597769bc39e56e73061a5c92382de253c1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-02-08 14:44:03 -0800

    Fix for hypergeometric calculations when num_drawn > num_black.
    Greatly fleshed out the tests to catch these scenarios.

--- tests/src/aggregation/HypergeometricTail.cpp
+++ tests/src/aggregation/HypergeometricTail.cpp
@@ -7,10 +7,6 @@
 TEST(HypergeometricTail, Basic) {
     scran::HypergeometricTail hyper;
 
-    // Checking special cases (remember that upper tail is the default).
-    EXPECT_EQ(hyper.run(0, 20, 30, 20), 1);
-    EXPECT_EQ(hyper.run(20, 20, 30, 20), 0);
-
     // Checking for consistency with R. We use a fairly generous tolerance
     // as our stirling approximation is not as accurate as R's.
 
@@ -26,14 +22,32 @@ TEST(HypergeometricTail, Basic) {
 
         // > phyper(20, 33, 8, 25, lower.tail=FALSE)
         compare_almost_equal(0.3743729, hyper.run(20, 33, 8, 25), /* tol=*/ 0.01);
+
+        // Check for correct behavior when num_black, num_white < num_drawn.
+        {
+            // > phyper(3, 5, 8, 10, lower.tail=FALSE)
+            compare_almost_equal(0.6853147, hyper.run(3, 5, 8, 10), /* tol=*/ 0.05);
+
+            // > phyper(13, 15, 18, 20, lower.tail=FALSE)
+            compare_almost_equal(0.000500776, hyper.run(13, 15, 18, 20), /* tol=*/ 0.05);
+        }
+
+        // Check for correct boundary case behavior when num_black == num_drawn or num_white == num_drawn.
+        {
+            // > phyper(20, 33, 25, 25, lower.tail=FALSE)
+            compare_almost_equal(0.0002843443, hyper.run(20, 33, 25, 25), /* tol=*/ 0.01);
+
+            // > phyper(20, 33, 25, 33, lower.tail=FALSE)
+            compare_almost_equal(0.1780022, hyper.run(20, 33, 25, 33), /* tol=*/ 0.01);
+
+            // > phyper(20, 25, 25, 25, lower.tail=FALSE)
+            compare_almost_equal(1.308459e-06, hyper.run(20, 25, 25, 25), /* tol=*/ 0.01);
+        }
     }
 
-    // Repeating with the lower tail.
-    hyper.set_upper_tail(false);
-    EXPECT_EQ(hyper.run(0, 20, 30, 20), 0);
-    EXPECT_EQ(hyper.run(20, 20, 30, 20), 1);
-
     {
+        hyper.set_upper_tail(false);
+
         // > phyper(3, 55, 101, 23)
         compare_almost_equal(0.01127538, hyper.run(3, 55, 101, 23), /* tol=*/ 0.01);
 
@@ -45,6 +59,9 @@ TEST(HypergeometricTail, Basic) {
 
         // > phyper(20, 33, 8, 25)
         compare_almost_equal(0.6256271, hyper.run(20, 33, 8, 25), /* tol=*/ 0.01);
+
+        // > phyper(20, 33, 25, 25)
+        compare_almost_equal(0.9997157, hyper.run(20, 33, 25, 25), /* tol=*/ 0.01);
     }
 }
 
@@ -53,18 +70,47 @@ TEST(HypergeometricTail, Logged) {
     auto lhyper = hyper;
     lhyper.set_log(true);
 
-    EXPECT_EQ(lhyper.run(0, 20, 30, 20), 0);
-    EXPECT_TRUE(std::isinf(lhyper.run(20, 20, 30, 20)));
     compare_almost_equal(std::log(hyper.run(3, 55, 101, 23)), lhyper.run(3, 55, 101, 23));
     compare_almost_equal(std::log(hyper.run(8, 19, 31, 14)), lhyper.run(8, 19, 31, 14));
 
     hyper.set_upper_tail(false);
     lhyper.set_upper_tail(false);
 
-    EXPECT_TRUE(std::isinf(lhyper.run(0, 20, 30, 20)));
-    EXPECT_EQ(lhyper.run(20, 20, 30, 20), 0);
     compare_almost_equal(std::log(hyper.run(3, 55, 101, 23)), lhyper.run(3, 55, 101, 23));
     compare_almost_equal(std::log(hyper.run(8, 19, 31, 14)), lhyper.run(8, 19, 31, 14));
+}
+
+TEST(HypergeometricTail, EdgeCases) {
+    {
+        scran::HypergeometricTail hyper;
+
+        // Checking special cases (remember that upper tail is the default).
+        EXPECT_EQ(hyper.run(0, 20, 30, 20), 1);
+        EXPECT_EQ(hyper.run(20, 20, 30, 20), 0);
+
+        // The number of drawn white balls must be at least 11 here, because
+        // there just aren't enough black balls; thus the probability of drawing
+        // more balls is 100%.
+        EXPECT_EQ(hyper.run(10, 20, 9, 20), 1);
+
+        // Repeating with the lower tail.
+        hyper.set_upper_tail(false);
+        EXPECT_EQ(hyper.run(0, 20, 30, 20), 0);
+        EXPECT_EQ(hyper.run(20, 20, 30, 20), 1);
+        EXPECT_EQ(hyper.run(10, 20, 9, 20), 0);
+    }
+
+    {
+        scran::HypergeometricTail hyper;
+        hyper.set_log(true);
+
+        EXPECT_EQ(hyper.run(0, 20, 30, 20), 0);
+        EXPECT_TRUE(std::isinf(hyper.run(20, 20, 30, 20)));
+
+        hyper.set_upper_tail(false);
+        EXPECT_TRUE(std::isinf(hyper.run(0, 20, 30, 20)));
+        EXPECT_EQ(hyper.run(20, 20, 30, 20), 0);
+    }
 }
 
 TEST(HypergeometricTail, Cached) {
@@ -101,5 +147,31 @@ TEST(HypergeometricTail, Cached) {
 
     // Cache breaking by changing the constants.
     EXPECT_EQ(hyper.run(8, num_whites + 5, num_blacks, num_draws), hyper.run(8, num_whites + 5, num_blacks, num_draws, cache));
+
+    // Checking that the cached method gives the same results when num_draws is greater than both num_blacks and num_whites.
+    {
+        auto cache = hyper.new_cache();
+        int num_whites = 15;
+        int num_blacks = 9;
+        int num_draws = 20;
+
+        EXPECT_EQ(hyper.run(11, num_whites, num_blacks, num_draws), hyper.run(11, num_whites, num_blacks, num_draws, cache));
+        EXPECT_EQ(hyper.run(12, num_whites, num_blacks, num_draws), hyper.run(12, num_whites, num_blacks, num_draws, cache));
+        EXPECT_EQ(hyper.run(15, num_whites, num_blacks, num_draws), hyper.run(15, num_whites, num_blacks, num_draws, cache));
+        EXPECT_EQ(hyper.run(10, num_whites, num_blacks, num_draws), hyper.run(10, num_whites, num_blacks, num_draws, cache));
+    }
+
+    // Checking that the cached method gives the same results when num_draws is equal to both num_blacks and num_whites.
+    {
+        auto cache = hyper.new_cache();
+        int num_whites = 15;
+        int num_blacks = 15;
+        int num_draws = 15;
+
+        EXPECT_EQ(hyper.run(11, num_whites, num_blacks, num_draws), hyper.run(11, num_whites, num_blacks, num_draws, cache));
+        EXPECT_EQ(hyper.run(12, num_whites, num_blacks, num_draws), hyper.run(12, num_whites, num_blacks, num_draws, cache));
+        EXPECT_EQ(hyper.run(15, num_whites, num_blacks, num_draws), hyper.run(15, num_whites, num_blacks, num_draws, cache));
+        EXPECT_EQ(hyper.run(10, num_whites, num_blacks, num_draws), hyper.run(10, num_whites, num_blacks, num_draws, cache));
+    }
 }
 
