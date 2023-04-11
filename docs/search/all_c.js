b813bf3fdd9822afd27f8a10769c9da662bbfd4f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-04-11 22:49:57 +0000

    Deploying to gh-pages from @ LTLA/libscran@088f543826e517484402e702917ca3da2126a6f0 🚀

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['quality_0',['quality',['../structscran_1_1ClusterSNNGraphLeiden_1_1Results.html#a997a0d553c10721ea74ffcafc5dfb742',1,'scran::ClusterSNNGraphLeiden::Results']]]
+  ['quality_0',['quality',['../structscran_1_1ClusterSNNGraphLeiden_1_1Results.html#a997a0d553c10721ea74ffcafc5dfb742',1,'scran::ClusterSNNGraphLeiden::Results']]],
+  ['quantile_1',['quantile',['../structscran_1_1ChoosePseudoCount_1_1Defaults.html#a15e134c0fd9cbf7b5fb879ff84d9605d',1,'scran::ChoosePseudoCount::Defaults']]]
 ];
--- docs/search/classes_2.js
+++ docs/search/classes_2.js
@@ -3,9 +3,10 @@ var searchData=
   ['centersizefactors_0',['CenterSizeFactors',['../classscran_1_1CenterSizeFactors.html',1,'scran']]],
   ['choosehvgs_1',['ChooseHVGs',['../classscran_1_1ChooseHVGs.html',1,'scran']]],
   ['chooseoutlierfilters_2',['ChooseOutlierFilters',['../structscran_1_1ChooseOutlierFilters.html',1,'scran']]],
-  ['clustersnngraphleiden_3',['ClusterSNNGraphLeiden',['../classscran_1_1ClusterSNNGraphLeiden.html',1,'scran']]],
-  ['clustersnngraphmultilevel_4',['ClusterSNNGraphMultiLevel',['../classscran_1_1ClusterSNNGraphMultiLevel.html',1,'scran']]],
-  ['clustersnngraphwalktrap_5',['ClusterSNNGraphWalktrap',['../classscran_1_1ClusterSNNGraphWalktrap.html',1,'scran']]],
-  ['combinations_6',['Combinations',['../structscran_1_1AggregateAcrossCells_1_1Combinations.html',1,'scran::AggregateAcrossCells']]],
-  ['computemedianmad_7',['ComputeMedianMad',['../classscran_1_1ComputeMedianMad.html',1,'scran']]]
+  ['choosepseudocount_3',['ChoosePseudoCount',['../classscran_1_1ChoosePseudoCount.html',1,'scran']]],
+  ['clustersnngraphleiden_4',['ClusterSNNGraphLeiden',['../classscran_1_1ClusterSNNGraphLeiden.html',1,'scran']]],
+  ['clustersnngraphmultilevel_5',['ClusterSNNGraphMultiLevel',['../classscran_1_1ClusterSNNGraphMultiLevel.html',1,'scran']]],
+  ['clustersnngraphwalktrap_6',['ClusterSNNGraphWalktrap',['../classscran_1_1ClusterSNNGraphWalktrap.html',1,'scran']]],
+  ['combinations_7',['Combinations',['../structscran_1_1AggregateAcrossCells_1_1Combinations.html',1,'scran::AggregateAcrossCells']]],
+  ['computemedianmad_8',['ComputeMedianMad',['../classscran_1_1ComputeMedianMad.html',1,'scran']]]
 ];
