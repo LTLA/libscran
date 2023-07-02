9c30c3b36b5611c885a8413a21def3ef61a020c7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-02 08:19:39 +0000

    Deploying to gh-pages from @ LTLA/libscran@02e5dd0ca5e6f1cba89b2b0c772d64b281a7f7c9 🚀

--- docs/search/all_12.js
+++ docs/search/all_12.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['weight_5fpolicy_0',['weight_policy',['../structscran_1_1ResidualPca_1_1Defaults.html#ad313b5573f8175ab1fb474481126bf41',1,'scran::ResidualPca::Defaults']]],
-  ['weightpolicy_1',['WeightPolicy',['../classscran_1_1ResidualPca.html#aa6ff98f2ed51c84448fead6dc615ccb4',1,'scran::ResidualPca']]],
+  ['weight_5fpolicy_0',['weight_policy',['../structscran_1_1ResidualPca_1_1Defaults.html#ad313b5573f8175ab1fb474481126bf41',1,'scran::ResidualPca::Defaults::weight_policy'],['../structscran_1_1ScoreFeatureSet_1_1Defaults.html#a697de1b2aa7d8fe7e2cd9c0630468827',1,'scran::ScoreFeatureSet::Defaults::weight_policy']]],
+  ['weightpolicy_1',['WeightPolicy',['../classscran_1_1ResidualPca.html#aa6ff98f2ed51c84448fead6dc615ccb4',1,'scran::ResidualPca::WeightPolicy'],['../classscran_1_1ScoreFeatureSet.html#aad2852f816d76bebe5a5ecec6a55d6ec',1,'scran::ScoreFeatureSet::WeightPolicy']]],
   ['weights_2',['weights',['../structscran_1_1ScoreFeatureSet_1_1Results.html#af3354e4458ab4a0e905b6580f93afb39',1,'scran::ScoreFeatureSet::Results']]]
 ];
--- docs/search/variables_11.js
+++ docs/search/variables_11.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['weight_5fpolicy_0',['weight_policy',['../structscran_1_1ResidualPca_1_1Defaults.html#ad313b5573f8175ab1fb474481126bf41',1,'scran::ResidualPca::Defaults']]],
+  ['weight_5fpolicy_0',['weight_policy',['../structscran_1_1ResidualPca_1_1Defaults.html#ad313b5573f8175ab1fb474481126bf41',1,'scran::ResidualPca::Defaults::weight_policy'],['../structscran_1_1ScoreFeatureSet_1_1Defaults.html#a697de1b2aa7d8fe7e2cd9c0630468827',1,'scran::ScoreFeatureSet::Defaults::weight_policy']]],
   ['weights_1',['weights',['../structscran_1_1ScoreFeatureSet_1_1Results.html#af3354e4458ab4a0e905b6580f93afb39',1,'scran::ScoreFeatureSet::Results']]]
 ];
