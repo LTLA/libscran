788b091174c41c02a78e27f3db1cd18edc6c5ec4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-07 15:30:07 +0000

    Deploying to gh-pages from @ LTLA/libscran@5cb1ee33489ac117161ec597c78ccb78aa28142d 🚀

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -5,7 +5,7 @@ var searchData=
   ['aggregateacrosscells_2ehpp_2',['AggregateAcrossCells.hpp',['../AggregateAcrossCells_8hpp.html',1,'']]],
   ['already_5fzeroed_3',['already_zeroed',['../structscran_1_1PerCellQcMetrics_1_1Buffers.html#abfc89c44c3c57da63654baeb3c571137',1,'scran::PerCellQcMetrics::Buffers']]],
   ['approximate_4',['approximate',['../structscran_1_1DownsampleByNeighbors_1_1Defaults.html#a217dc2cf9a3fb84e137360a078b0ea49',1,'scran::DownsampleByNeighbors::Defaults::approximate'],['../structscran_1_1ScaleByNeighbors_1_1Defaults.html#a8659f7467c8c29d61c1317957e1b20b6',1,'scran::ScaleByNeighbors::Defaults::approximate']]],
-  ['auc_5',['auc',['../structscran_1_1PairwiseEffects_1_1Results.html#a1fba82e77880d1a222f452f4843c399a',1,'scran::PairwiseEffects::Results::auc'],['../structscran_1_1ScoreMarkers_1_1Results.html#a20c97e99ae435f1de0ecbc71fab5dd55',1,'scran::ScoreMarkers::Results::auc']]],
+  ['auc_5',['auc',['../structscran_1_1PairwiseEffects_1_1Results.html#a1fba82e77880d1a222f452f4843c399a',1,'scran::PairwiseEffects::Results::auc'],['../structscran_1_1ScoreMarkers_1_1Results.html#aac7d3f24ac83452d5e003e0026e39089',1,'scran::ScoreMarkers::Results::auc']]],
   ['average_6',['average',['../structscran_1_1ModelGeneVar_1_1AverageBlockResults.html#a873a8153dcc6dc498dee357fb2b82294',1,'scran::ModelGeneVar::AverageBlockResults']]],
   ['average_5fvectors_7',['average_vectors',['../namespacescran.html#a1ad83ffd5170468e5c4ec082605f79e7',1,'scran::average_vectors(size_t n, std::vector&lt; Stat * &gt; in, Output *out)'],['../namespacescran.html#a824f746e361b0d57b2bb756fd7713827',1,'scran::average_vectors(size_t n, std::vector&lt; Stat * &gt; in)']]],
   ['average_5fvectors_2ehpp_8',['average_vectors.hpp',['../average__vectors_8hpp.html',1,'']]],
