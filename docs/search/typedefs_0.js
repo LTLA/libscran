a2c57b589cfd163f9a4c8d94bfb13a04213bd297
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-18 19:34:58 +0000

    Deploying to gh-pages from @ LTLA/libscran@62e2745d51463c364f7113eeae53b6e473e511ff 🚀

--- docs/search/typedefs_0.js
+++ docs/search/typedefs_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['clusterkmeans_0',['ClusterKmeans',['../namespacescran.html#a836b35676870dd7296db5763e317c702',1,'scran']]],
-  ['computesummaries_1',['ComputeSummaries',['../classscran_1_1ScoreMarkers.html#a836ea0c31fa496f274943e79035314b5',1,'scran::ScoreMarkers']]]
+  ['computesummaries_0',['ComputeSummaries',['../classscran_1_1ScoreMarkers.html#a836ea0c31fa496f274943e79035314b5',1,'scran::ScoreMarkers']]]
 ];
