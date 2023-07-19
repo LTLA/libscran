ac5c14ea56d51cac126de85ee1c532cdd365b893
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-19 23:00:00 +0000

    Deploying to gh-pages from @ LTLA/libscran@591bd6fabd573ad5762c8efea257a114821c92c9 🚀

--- docs/search/classes_0.js
+++ docs/search/classes_0.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['aggregateacrosscells_0',['AggregateAcrossCells',['../classscran_1_1AggregateAcrossCells.html',1,'scran']]],
-  ['averageblockresults_1',['AverageBlockResults',['../structscran_1_1ModelGeneVariances_1_1AverageBlockResults.html',1,'scran::ModelGeneVariances']]]
+  ['aggregateacrosscells_0',['AggregateAcrossCells',['../classscran_1_1AggregateAcrossCells.html',1,'scran']]]
 ];
