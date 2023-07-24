0ecf3abbd784e13ce17b479a6824ed05a7c648a6
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-24 21:32:05 +0000

    Deploying to gh-pages from @ LTLA/libscran@ce7a45088b01ff935f7302cb9db3d2ab06a0b67a 🚀

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['handle_5fzeros_0',['handle_zeros',['../structscran_1_1LogNormCounts_1_1Defaults.html#a0b620ea5b5046b58c470297d621647dd',1,'scran::LogNormCounts::Defaults']]],
-  ['hypergeometrictail_1',['HypergeometricTail',['../classscran_1_1HypergeometricTail.html',1,'scran']]],
-  ['hypergeometrictail_2ehpp_2',['HypergeometricTail.hpp',['../HypergeometricTail_8hpp.html',1,'']]]
+  ['has_5fnon_5ffinite_1',['has_non_finite',['../structscran_1_1CenterSizeFactors_1_1Results.html#a42cbf5672ce9a9526db7e4ea9ca1bb26',1,'scran::CenterSizeFactors::Results']]],
+  ['has_5fzero_2',['has_zero',['../structscran_1_1CenterSizeFactors_1_1Results.html#a6114ca8d84883a7aedb2319da194a4a9',1,'scran::CenterSizeFactors::Results']]],
+  ['hypergeometrictail_3',['HypergeometricTail',['../classscran_1_1HypergeometricTail.html',1,'scran']]],
+  ['hypergeometrictail_2ehpp_4',['HypergeometricTail.hpp',['../HypergeometricTail_8hpp.html',1,'']]]
 ];
