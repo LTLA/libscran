3d0cfcb8f979f1b976f7b727377ff34040f6e4cc
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-25 00:07:36 +0000

    Deploying to gh-pages from @ LTLA/libscran@107924c09fb9f7b00761426bd5ba7fa15f2d1478 🚀

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -1,7 +1,7 @@
 var searchData=
 [
-  ['handle_5fnon_5ffinite_0',['handle_non_finite',['../structscran_1_1LogNormCounts_1_1Defaults.html#a6dd5eaf3b2c9b66e478ccfae07974d27',1,'scran::LogNormCounts::Defaults']]],
-  ['handle_5fzeros_1',['handle_zeros',['../structscran_1_1LogNormCounts_1_1Defaults.html#a0b620ea5b5046b58c470297d621647dd',1,'scran::LogNormCounts::Defaults']]],
+  ['handle_5fnon_5ffinite_0',['handle_non_finite',['../structscran_1_1GroupedSizeFactors_1_1Defaults.html#a0586f933b7cd61f9f07daa8272360d2a',1,'scran::GroupedSizeFactors::Defaults::handle_non_finite'],['../structscran_1_1LogNormCounts_1_1Defaults.html#a6dd5eaf3b2c9b66e478ccfae07974d27',1,'scran::LogNormCounts::Defaults::handle_non_finite']]],
+  ['handle_5fzeros_1',['handle_zeros',['../structscran_1_1GroupedSizeFactors_1_1Defaults.html#a95d70f82976a430a11c20c1e9a5a068e',1,'scran::GroupedSizeFactors::Defaults::handle_zeros'],['../structscran_1_1LogNormCounts_1_1Defaults.html#a0b620ea5b5046b58c470297d621647dd',1,'scran::LogNormCounts::Defaults::handle_zeros']]],
   ['has_5fnon_5ffinite_2',['has_non_finite',['../structscran_1_1CenterSizeFactors_1_1Results.html#a42cbf5672ce9a9526db7e4ea9ca1bb26',1,'scran::CenterSizeFactors::Results']]],
   ['has_5fzero_3',['has_zero',['../structscran_1_1CenterSizeFactors_1_1Results.html#a6114ca8d84883a7aedb2319da194a4a9',1,'scran::CenterSizeFactors::Results']]],
   ['hypergeometrictail_4',['HypergeometricTail',['../classscran_1_1HypergeometricTail.html',1,'scran']]],
