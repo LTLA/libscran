19cd3f94318e9462b8fd4dc68d190a86e1951bf4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-16 00:14:46 +0000

    Deploying to gh-pages from @ LTLA/libscran@dd005d298656a4ba9336845a26e0a01242cc1624 🚀

--- docs/search/variables_7.js
+++ docs/search/variables_7.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['ignore_5fzeros_0',['ignore_zeros',['../structscran_1_1CenterSizeFactors_1_1Defaults.html#a818565bd477ef479ae6a557d54ed4a47',1,'scran::CenterSizeFactors::Defaults']]],
-  ['iterations_1',['iterations',['../structscran_1_1ClusterSnnGraphLeiden_1_1Defaults.html#a1f20a3d903a70ab7fb88577a22300bd1',1,'scran::ClusterSnnGraphLeiden::Defaults']]]
+  ['intersect_1',['intersect',['../structscran_1_1FilterCells_1_1Defaults.html#aabec8e701006b605f5f232d6a0dc3e4a',1,'scran::FilterCells::Defaults']]],
+  ['iterations_2',['iterations',['../structscran_1_1ClusterSnnGraphLeiden_1_1Defaults.html#a1f20a3d903a70ab7fb88577a22300bd1',1,'scran::ClusterSnnGraphLeiden::Defaults']]]
 ];
