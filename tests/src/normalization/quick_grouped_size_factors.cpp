fe72ed8d9d1a51e4b3047a1962e7b9d2f1a9703f
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-09-18 12:43:48 -0700

    Further fixes.

--- tests/src/normalization/quick_grouped_size_factors.cpp
+++ tests/src/normalization/quick_grouped_size_factors.cpp
@@ -21,37 +21,6 @@ protected:
         auto vec = sim.vector(NR * NC);
         ptr.reset(new tatami::DenseRowMatrix<double, int>(NR, NC, std::move(vec)));
     }
-
-    size_t num_groups = 10;
-    std::vector<double> true_factors;
-    void sanity_assemble() {
-        Simulator sim;
-        sim.lower = 1;
-        sim.upper = 10;
-        sim.density = 1;
-        true_factors = sim.vector(NC);
-
-        sim.seed = 12345;
-        auto gene_abundances = sim.vector(NR);
-        size_t group_size = NC / num_groups;
-        size_t de_per_group = NR / num_groups;
-
-        std::vector<double> vec(NR * NC);
-        for (size_t c = 0; c < NC; ++c) {
-            size_t offset = c * NR;
-            for (size_t r = 0; r < NR; ++r) {
-                vec[offset + r] = gene_abundances[r] * true_factors[c];
-            }
-
-            size_t group = c / group_size;
-            size_t de_offset = group * de_per_group; 
-            for (size_t r = 0; r < de_per_group; ++r) {
-                vec[offset + de_offset + r] = 10000 * true_factors[c];
-            }
-        }
-
-        ptr.reset(new tatami::DenseColumnMatrix<double, int>(NR, NC, std::move(vec)));
-    }
 };
 
 TEST_F(quick_grouped_size_factors_Test, Simple) {
@@ -106,7 +75,39 @@ TEST_F(quick_grouped_size_factors_Test, Blocked) {
 }
 
 TEST_F(quick_grouped_size_factors_Test, Sanity) {
-    sanity_assemble();
+    size_t num_groups = 10;
+    std::vector<double> true_factors;
+
+    {
+        std::mt19937_64 rng(12345);
+        for (size_t c = 0; c < NC; ++c) {
+            true_factors.push_back(aarand::standard_uniform(rng) * 1.5 + 0.5);
+        }
+
+        std::vector<double> gene_abundances;
+        for (size_t r = 0; r < NR; ++r) {
+            gene_abundances.push_back(aarand::standard_uniform(rng) * 10);
+        }
+
+        size_t group_size = NC / num_groups;
+        size_t de_per_group = NR / num_groups;
+
+        std::vector<double> vec(NR * NC);
+        for (size_t c = 0; c < NC; ++c) {
+            size_t offset = c * NR;
+            for (size_t r = 0; r < NR; ++r) {
+                vec[offset + r] = gene_abundances[r] * true_factors[c] + aarand::standard_uniform(rng) * 0.01; // add a little jitter, otherwise the PCA freaks out.
+            }
+
+            size_t group = c / group_size;
+            size_t de_offset = group * de_per_group; 
+            for (size_t r = 0; r < de_per_group; ++r) {
+                vec[offset + de_offset + r] = 1000 * true_factors[c];
+            }
+        }
+
+        ptr.reset(new tatami::DenseColumnMatrix<double, int>(NR, NC, std::move(vec)));
+    }
 
     scran::quick_grouped_size_factors::Options opt;
     opt.clusters = [&](size_t) -> size_t { return num_groups; }; // setting it exactly for simplicity in an exact test.
@@ -121,17 +122,14 @@ TEST_F(quick_grouped_size_factors_Test, Sanity) {
     auto max_ratio = *std::max_element(ratios.begin(), ratios.end());
     EXPECT_TRUE(min_ratio > 0);
     EXPECT_TRUE(max_ratio > 0);
-    EXPECT_TRUE(std::abs(min_ratio - max_ratio) < 1e-6);
+    EXPECT_TRUE(std::abs(min_ratio - max_ratio) < 0.01); // very close to each other.
 
-    // Works when blocked. (Make sure the number of clusters is even
-    // otherwise the PCA partitioning can do crazy things in this
-    // highly contrived example where all points lie on the center.)
+    // Works when blocked. 
     std::vector<int> block;
-    block.insert(block.end(), 80, 0);
-    block.insert(block.end(), ptr->ncol() - 80, 1);
+    block.insert(block.end(), ptr->ncol() / 2, 0);
+    block.insert(block.end(), ptr->ncol() / 2, 1);
 
-    size_t group_size = NC / num_groups;
-    opt.clusters = [&](size_t n) -> size_t { return n / group_size; }; // setting it exactly for simplicity.
+    opt.clusters = [&](size_t n) -> size_t { return num_groups / 2; }; // setting it exactly for simplicity.
     opt.block = block.data();
     auto blocked = scran::quick_grouped_size_factors::run(ptr.get(), opt);
     EXPECT_EQ(out, blocked);
